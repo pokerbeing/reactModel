b256be1674448f04d807e52081768c62819f6347
PokerBeing <pokerbeing@yahoo.com>
2022-08-25 02:58:36 +0000

    Close button working

--- src/App.js
+++ src/App.js
@@ -1,44 +1,42 @@
 import React from 'react';
 import "./styles.css";
 
-const Child = () => (
-<div className='modal'>
-      Hello, World!
-  </div>
+const Child = props => (
+    <div id="myModal" className="modal">
+        <div className="modal-content">
+            <span className="close">&times;</span>
+            <p>Enter TODO below:</p>
+            <input type="text" id="prompt" size="80"></input>
+            <button onClick={props.hideToggle}>
+              Close
+            </button>
+        </div>
+    </div> 
 )
 
 export default class Modal extends React.Component {
     constructor(){
         super()
         this.state = {
-            isHidden: false,
+            isHidden: true,
         }
     }
     
-    toggleVisibility() {
-        this.setState({
-            isHidden: !this.state.isHidden
-        })
-    }
+    toggleHidden () {
+    this.setState({
+      isHidden: !this.state.isHidden
+    })
+  }
     
     render() {
         return (
             <div>
             <h1>Hello CodeSandbox</h1>
             <h2>Start editing to see some magic happen!</h2>
-            <button  onClick={() => this.toggleVisibility()}>Modal</button>
-            {!this.state.isHidden && <Child />}
-
-            {/* Modal content -- From w3schools.com -- with some modifications by me    
-                <div id="myModal" className="modal">
-                    <div className="modal-content">
-                        <span className="close">&times;</span>
-                        <p>Enter TODO below:</p>
-                        <input type="text" id="prompt" size="80"></input>
-                    </div>
-                </div> 
-                             */}
-
+            <button onClick={() => this.toggleHidden()} >
+              Click to show modal
+            </button>
+            {!this.state.isHidden && <Child hideToggle={() => this.toggleHidden()} />}
             </div>
         );
     }   

670a435667763282a7448667f0ffd8117e09bd65
PokerBeing <pokerbeing@yahoo.com>
2022-08-25 01:55:53 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
+import React from 'react';
 import "./styles.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+const Child = () => (
+<div className='modal'>
+      Hello, World!
+  </div>
+)
+
+export default class Modal extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            isHidden: false,
+        }
+    }
+    
+    toggleVisibility() {
+        this.setState({
+            isHidden: !this.state.isHidden
+        })
+    }
+    
+    render() {
+        return (
+            <div>
+            <h1>Hello CodeSandbox</h1>
+            <h2>Start editing to see some magic happen!</h2>
+            <button  onClick={() => this.toggleVisibility()}>Modal</button>
+            {!this.state.isHidden && <Child />}
+
+            {/* Modal content -- From w3schools.com -- with some modifications by me    
+                <div id="myModal" className="modal">
+                    <div className="modal-content">
+                        <span className="close">&times;</span>
+                        <p>Enter TODO below:</p>
+                        <input type="text" id="prompt" size="80"></input>
+                    </div>
+                </div> 
+                             */}
+
+            </div>
+        );
+    }   
 }
